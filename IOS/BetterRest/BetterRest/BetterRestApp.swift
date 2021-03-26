34b0936b617e8195e2932c20e41a9fe1879ebfe8
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2021-03-26 15:58:05 +0100

    Create BetterRest App
    The app uses machine learning to find out when a use should go to sleep based on wake up time, estimated sleep and coffee amount

---
+++ IOS/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rawan Abou Dehn on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
